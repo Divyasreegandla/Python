32fd73e061e70da55942cf1766e422a74bb073fa
Divyasreegandla <divyasreegandla2000@gmail.com>
2026-02-20 12:15:34 +0530

    first commit

---
+++ Intro.py
@@ -0,0 +1,41 @@
+print(123)
+print(456)
+
+#single line comment
+#the below code will not execute
+#Print(12)
+
+#multi line comment
+'''jhgfdcvbnmmnb
+jhgfxcvbn'''
+
+#variables
+
+client_id=1235678
+print(client_id)
+
+#data types
+#(type function)
+
+a=10
+print(type(a))
+
+b=1.0
+print(type(b))
+
+c="566"
+print(type(c))
+
+print(a+b)
+print("ugfv"+c) #concatination
+
+# print("fdgg"+45) #error
+
+name="Yash"
+age=33
+
+#his name is yash.he is 33 years old
+#type casting str(age)
+
+print("His name is "+name+". He is "+str(age)+" years old.")
+
---
+++ Operators.py
@@ -0,0 +1,68 @@
+# a=12
+# b=15
+#arthemetic
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+# print(77/0)
+# print(0/7)
+
+#comparision
+
+# a=16
+# b=17
+# print(a==b)
+# print(a==b-1)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+#logical
+
+# a=False
+# b=True
+# print(a and b)
+# print(a or b)
+# print(not b)
+
+# a=15
+# b=17
+
+# print(5+4-3*6/2)
+
+# print((a>b-2) and ((a**2>b*2**4) or not (a<b/2)))
+
+#Assignment 
+
+# a=10
+# print(a)
+# a+=2
+# print(a)
+# a-=4
+# print(a)
+# a*=10
+# print(a)
+# a/=8
+# print(a)
+# a//=3
+# print(a)
+# a%=13
+# print(a)
+# a**=2
+# print(a)
+
+#Bitwise operators
+
+# print(9&19)
+# print(9 | 19)
+# print(24^22)
+# print(16<<3)
+# print(16>>3)
+# print(~19)
+# print(~-11)
