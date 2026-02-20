0d88f6a1c948ec6547be2b2827db16567b5aa848
Divyasreegandla <divyasreegandla2000@gmail.com>
2026-02-20 20:58:07 +0530

    task

---
+++ Task.py
@@ -0,0 +1,155 @@
+# # print("Hello World, Welcome to Python")
+
+# # num=10 #varaible declaration can start with letters
+# # _a=20  # _ can be used at starting
+# # print(_a)
+# # a_b=30 # _ can be used to seperate two wods
+# # print(a_b)
+
+# #  #var=20 
+# # #  $salary=4
+# # # 11num=34
+# # # var 1=45
+# # '''space cannot be used at the starting
+# # it throws an error
+# #  '''
+# # Python=1
+# # print(Python)
+
+# # var1=9
+# # print(var1)
+# #-------------------------------
+# #int
+
+# a=100
+# num_data=20
+
+# #float
+
+# b=20.5/num_data
+# print(b)
+
+# #string
+
+# name="Divya"
+# name1="Sree"
+# fullname="My name"+" "+"is "+name+" "+name1 # cancatenation
+# print(fullname)
+# print("I am "+"working in Stackly"+" "+"as a Associate Software Engineer")
+
+# #boolean
+
+# c=5
+# d=6
+# print(a>b)
+# print(a==b)
+
+# #Type casting
+# # -----------------------
+
+# age=25
+# print("i am "+str(age)+"years old")
+
+# amount=299.9
+# print(int(amount))
+
+# num="60"
+# print(int(num))
+# print(float(num))
+
+# print(bool(num))
+# print(bool(amount))
+# print(bool(0))
+# print(bool(""))
+
+#Operators
+# a=26
+# b=39
+# # Addition ---> +
+# print(a+b)
+# # Subtraction ---> - 
+# print(a-b)
+# # Multiplication ---> *
+# print(a*b)
+# # Division ---> /
+# print(a/b)
+# # Floor Division ---> //
+# print(a//b)
+# # Modulus ---> % (reminder)
+# print(a%b)
+# # Exponentiation ---> ** 
+# print(a**b)
+
+# Comparison Operators (Boolean)
+# --------------------
+
+# a=25
+# b=30
+# # == equal to
+# print(a==b)
+# # != not equal to
+# print(a!=b)
+# # < lesser than 
+# print(a<b)
+# # > greater than
+# print(a>b) 
+# # <= less or equal to
+# print(a<=b)
+# # >= greater or equal to
+# print(a>=b)
+
+# Logical Operators
+# ------------------
+
+# a=True
+# b=False
+# # AND --> and 
+# print(a and b)
+# # OR --> or
+# print(a or b)
+# # NOT --> not 
+# print(not a)
+# print(not b)
+
+# # Assignment operators:
+
+# # = assign
+# a=20
+# # += add and assign
+# a+=5
+# print(a)
+# # -= a sub and assign
+# a-=5
+# print(a)
+# # *=
+# a*=5
+# print(a)
+# # /=
+# a/=5
+# print(a)
+# # //=
+# a//=5
+# print(a)
+# # %=
+# a%=5
+# print(a)
+# # **=
+# a**=5
+# print(a)
+
+# Bitwise Operator
+
+# and---> &
+print(5&12)
+# or--> |
+print(5|12)
+# x-or ---> ^
+print(5^12)
+# not --> ~
+print(~12)
+# right shift --->  >>
+print(4>>2)
+# left shift---> <<
+print(4<<2)
+
+
