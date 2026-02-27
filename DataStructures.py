45b5b90b75befc46a6cf324f31faf21cbf725b35
Divyasreegandla <divyasreegandla2000@gmail.com>
2026-02-27 12:35:07 +0530

    datastructures

---
+++ DataStructures.py
@@ -0,0 +1,42 @@
+# a="Hello World"
+# print(a[-3])
+
+# a="hello guys!!!!" 
+# print(a[-2:-4)
+# print(a[6:])
+# print(a[:6])
+# print(a[2:7])
+# print(a[::-1]) #reverse the output
+
+#List
+
+# list1=[1,2,3,4,"Hello","Hi",True,False,22,33,44,[22,33,44]]
+# print(list1)
+# print(len(list1))
+# print(list1[4])
+# print(list1[3:7]) 
+# print(list1[0:7:2])
+
+# list1[3]+=2
+# print(list1)
+# print(len(list1))
+
+list2=[213,234,234,234,3,5,234]
+# list2.append([3,4,5])
+# list2.append("Hello")
+# list2.extend([5,6,7,8])
+# list2.append("Hello")
+# list2.extend("Hello")
+# list2.insert(4,"hello")
+# list2.remove(234)
+# list2.pop()
+# list2.clear()
+# list2.sort() # asending
+# list2.sort(reverse=True) # decending
+# list2.reverse()
+# print(list2[::-1])
+# list2.reverse()
+# print(list2)
+# print(len(list2))
+print(list2[-4:-2])
+print(list2[-2:-4])
